b60e2674665893f380448a09b1e8e5695ab4a02a
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-26 16:34:20 +0200

    produit

--- front/js/product.js
+++ front/js/product.js
@@ -3,19 +3,15 @@ const searchParam = new URLSearchParams(link.search);
 const id = searchParam.get("id");
 if(!searchParam.has("id"))window.location = window.location.origin+"/front/html";
 
-const linkEnsembleProduits = "http://localhost:3000/api/products/";
+const linkProduit = "http://localhost:3000/api/products/"+id;
 
-let jsonProduits,currentProduct;
+let currentProduct;
 
 fetch(linkEnsembleProduits).then(response=>response.json()).then((data)=>{
-    jsonProduits = data;
-    let prods = jsonProduits.filter((product)=>product._id == id);
-    if(prods.length <= 0)window.location = window.location.origin+"/front/html";
-    currentProduct = prods[0];
+    currentProduct = data;
     console.log(currentProduct);
 }).catch((error)=>{
     console.error("error",error);
-    jsonProduits = [];
     currentProduct = null;
-    console.log(jsonProduits);
+    console.log(currentProduct);
 });
