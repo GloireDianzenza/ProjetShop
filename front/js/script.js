5c4e519e3fe80c821f1d0ad244b5302b78dd2f40
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-26 15:18:03 +0200

    produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,10 @@
 const linkEnsembleProduits = "http://localhost:3000/api/products/";
+
+let jsonProduits;
+
+fetch(linkEnsembleProduits).then(response=>response.json()).then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.error(error);
+    jsonProduits = [];
+})
