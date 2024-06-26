73d2013b79798db58430fc424f199602ab2a823a
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-26 16:00:48 +0200

    accueil

--- front/js/script.js
+++ front/js/script.js
@@ -4,9 +4,9 @@ let jsonProduits;
 
 fetch(linkEnsembleProduits).then(response=>response.json()).then((data)=>{
     jsonProduits = data;
-    console.log(jsonProduits);
+    
 }).catch((error)=>{
     console.error("error",error);
     jsonProduits = [];
-    console.log(jsonProduits);
+    items.innerHTML = "";
 });
