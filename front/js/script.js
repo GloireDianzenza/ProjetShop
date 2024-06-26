a518c4809ec7e87d88f68f1064aebd4cbe709b93
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-26 16:15:19 +0200

    accueil

--- front/js/script.js
+++ front/js/script.js
@@ -4,7 +4,32 @@ let jsonProduits;
 
 fetch(linkEnsembleProduits).then(response=>response.json()).then((data)=>{
     jsonProduits = data;
-    
+    console.log(jsonProduits);
+    for(let produit of jsonProduits){
+        console.log(produit);
+        let productLink = document.createElement("a");
+        productLink.href = "./product.html?id="+produit._id;
+
+        let article = document.createElement("article");
+
+        let image = document.createElement("img");
+        image.alt = produit.altTxt+", "+produit.name;
+        image.src = produit.imageUrl;
+        article.appendChild(image);
+        let productName = document.createElement("h3");
+        productName.classList.add("productName");
+        productName.innerHTML = produit.name;
+        article.appendChild(productName);
+        
+        let productDesc = document.createElement("p");
+        productDesc.classList.add("productDescription");
+        productDesc.innerHTML = produit.description;
+        article.appendChild(productDesc);
+
+        productLink.appendChild(article);
+
+        items.appendChild(productLink);
+    }
 }).catch((error)=>{
     console.error("error",error);
     jsonProduits = [];
