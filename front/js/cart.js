1e265e2006079b436090cc9569e2c3b7575a848c
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-26 19:13:13 +0200

    dd

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,9 +4,30 @@ let jsonProduits;
 
 fetch(linkEnsembleProduits).then(response=>response.json()).then((data)=>{
     jsonProduits = data;
-    console.log(jsonProduits);
 }).catch((error)=>{
     console.error("error",error);
     jsonProduits = [];
     console.log(jsonProduits);
 });
+
+console.log(localStorage);
+let cartItems = document.getElementById("cart__items");
+console.log(cartItems);
+let array = JSON.parse(localStorage.getItem("array"));
+let index = 0;
+for(let item of array){
+    let article = document.createElement("article");
+    article.classList.add("cart__item");
+    article.setAttribute("data-id",item.id);
+    article.setAttribute("data-color",item.color);
+
+    let imageDiv = document.createElement("div");
+
+    let image = document.createElement("img");
+    imageDiv.appendChild(image);
+    imageDiv.classList.add("cart__item__img");
+    article.appendChild(imageDiv);
+
+    cartItems.appendChild(article);
+    index++;
+}
